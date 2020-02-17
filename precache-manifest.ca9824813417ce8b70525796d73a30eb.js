595daa61ad4a02ce45be0a0f8ae41a43087daaa9
Maxim Yarmoliuk <jarmoluk97@gmail.com>
2020-02-17 10:40:41 +0200

    Updates

---
+++ precache-manifest.ca9824813417ce8b70525796d73a30eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a2f0ef2cdde6738c3ec053d5b01431",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9a912e14c2c73b81b4c1",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.d603b3c3.chunk.css"
+  },
+  {
+    "revision": "098740b640907fa3602a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ced87cf8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ced87cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a912e14c2c73b81b4c1",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e161781c.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
